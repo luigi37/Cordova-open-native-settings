e591dd5d1be3e5e2dfcd70fb5355f7b3c6b3f48d
luigi37 <luigi37@users.noreply.github.com>
2017-11-06 21:51:01 +0100

    Update NativeSettings.java

--- src/android/NativeSettings.java
+++ src/android/NativeSettings.java
@@ -14,6 +14,7 @@ import org.json.JSONException;
 import android.content.Intent;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 
 import android.provider.Settings;
 
